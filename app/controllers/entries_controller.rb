cd67d90e7b5ef02e38a5aaf35817c560faac9036
valentin-hc <valentin.hecqcauquil@gmail.com>
2015-11-04 12:39:19 +0100

    Add new entry adder linked with id_project

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,4 +7,30 @@ class EntriesController < ApplicationController
 			@entries = @project.entries
 		end
 	end
+	def new
+		@project = Project.find_by(id: params[:project_id])
+		@entry = Entry.new
+		#@entry = @project.entries.new
+	end
+	def create
+		project_id = params[:project_id]
+		project = Project.find_by(id: project_id)
+		entry = project.entries.new({
+			hours: params[:entry][:hours],
+			minutes: params[:entry][:minutes],
+			date: params[:entry][:date],
+			comments: params[:entry][:comments]
+			})
+		entry.save
+		redirect_to "/projects/#{project_id}/entries"
+	end
+	# def create
+	# 	entry = Entry.new
+	# 	if entry.save
+	# 		redirect_to
+	# 	else
+	# 		@errors = entry.errors.full_messages
+	# 		render "new"
+	# 	end
+	# end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   post "/projects" => "projects#create"
   get "/projects/new" => "projects#new" #order matters !
   get "/projects/:project_id/entries" =>"entries#index"
+  get "/projects/:project_id/entries/new" => "entries#new"
   get "/projects/:id" => "projects#show"
+  post "/projects/:project_id/entries" => "entries#create", as: "project_entries"
 end
